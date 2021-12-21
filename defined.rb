2c885bfc703bdcee29606b683017aa67685ce888
Bogdan <96384611+rhater02@users.noreply.github.com>
2021-12-21 21:34:36 +0300

    Add files via upload

---
+++ defined.rb
@@ -0,0 +1,13 @@
+a = 0
+
+if defined?(a)
+    puts "Определена переменная a"
+else
+    puts "Переменная a не определена"
+end
+
+if defined?(b)
+    puts "Определена переменная b"
+else
+    puts "Переменная b не определена"
+end
